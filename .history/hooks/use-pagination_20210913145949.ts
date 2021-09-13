581c4781b06d7983380e0bbd29dddbc28d0001a8
Moatez <moatez.bejaoui@esprit.tn>
2021-09-13 20:44:23 +0100

    add readme file

--- .history/hooks/use-pagination_20210913145949.ts
+++
@@ -1,6 +0,0 @@
-import { useState } from "react";
-
-const usePagination = <T>(data: T, page: number, limit: number): T => {
-  const [paginatedData, setPaginatedData] = useState([]);
-  return paginatedData;
-};
